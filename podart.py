2cbc29e4b8c115c606c21f95cde088e9a3356852
TrigonaMinima <shivam_r@outlook.com>
2021-05-10 01:16:26 +0530

    Add: Automatically update podarts (synced from Apple podcasts)

---
+++ podart.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from py.yamlproc import yaml2dict
+from py.podart import get_art_url, tinypng_compress, save_img
+
+
+if __name__ == "__main__":
+    posts_dir = Path("_posts")
+
+    for pod_f in posts_dir.iterdir():
+        print(pod_f)
+
+        pod_dets = yaml2dict(pod_f)
+        # print(pod_dets)
+
+        podart_path = Path(pod_dets["image"])
+        podart_url = get_art_url(pod_dets["apple_pod"])
+
+        podart = tinypng_compress(podart_url)
+
+        save_img(podart, podart_path)
+        print("Saved the compressed image at:", podart_path)
