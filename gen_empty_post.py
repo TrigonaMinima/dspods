cd6061fc2d34a526af39518f27cb28c479213ae1
TrigonaMinima <shivam_r@outlook.com>
2021-05-22 18:33:53 +0530

    Update: Add podcast name to podcast file template

--- gen_empty_post.py
+++ gen_empty_post.py
@@ -4,11 +4,12 @@ import datetime
 from pathlib import Path
 
 
+pod_name = sys.argv[1] if len(sys.argv) > 1 else "temp"
 template = f"""---
 layout: post
-title: ""
+title: "{pod_name}"
 categories: []
-image: assets/images/pods/
+image: assets/images/pods/{pod_name}.webp
 description: ""
 podurl:
 rss:
@@ -25,14 +26,12 @@ duration:
 status:
 ---
 """
-
 print(template)
-pod_name = sys.argv[1] if len(sys.argv) > 1 else "temp"
+
+
 today = datetime.datetime.today().strftime("%Y-%m-%d")
 file_name = f"{today}-{pod_name}.md"
 file_path = Path("_posts") / file_name
-
-
 with open(file_path, "w") as f:
     f.write(template)
 print("Created empty podcast file-", file_path)
